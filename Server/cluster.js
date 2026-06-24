4b9359286d3f0a89ccb3b42e4bda7f14628c1243
Nikki Chaurasia <nikkichaurasia422@gmail.com>
2026-06-24 17:09:18 +0530

    Fix cluster import

--- Server/cluster.js
+++ Server/cluster.js
@@ -1,20 +1,25 @@
-import cluster, { worker } from 'cluster';
-import os from 'os';
+import cluster from "cluster";
+import os from "os";
 
 const totalCPUs = os.cpus().length;
 
-if(cluster.isPrimary){
-    console.log(`Master ${process.pid} running`);
-    
-    for(let i=0; i<totalCPUs; i++){
-        cluster.fork();
-    }
-    
-    cluster.on("exit", (worker) => {
-        console.log(`Worker ${worker.process.pid} died`);
-        
-        cluster.fork();
-    })
-}else{
-    import('./server.js');
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} running`);
+
+  for (let i = 0; i < totalCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(
+      `Worker ${worker.process.pid} died (code: ${code}, signal: ${signal})`
+    );
+
+    console.log("Starting a new worker...");
+    cluster.fork();
+  });
+} else {
+  console.log(`Worker ${process.pid} started`);
+
+  await import("./server.js");
 }
