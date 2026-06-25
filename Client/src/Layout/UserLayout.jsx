66223973444d8db50b7f252984ba6cc063c03780
Nikki Chaurasia <nikkichaurasia422@gmail.com>
2026-06-25 16:03:58 +0530

    Fix Header import path casing

--- Client/src/Layout/UserLayout.jsx
+++ Client/src/Layout/UserLayout.jsx
@@ -1,6 +1,6 @@
 
 import { Outlet } from "react-router-dom";
-import Header from "../Components/Header/header";
+import Header from "../Components/Header/Header";
 import Footer from "../Components/Footer/Footer";
 
 function UserLayout() {
